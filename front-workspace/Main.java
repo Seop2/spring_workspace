936b44bb26ed84fd2568714eee26e25e39f4820a
Seop2 <57839278+Seop2@users.noreply.github.com>
2022-09-20 17:31:52 +0900

    Add files via upload

---
+++ front-workspace/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int age = 22;
+		String name ="홍길동";
+		double height = 185.5;
+		
+		System.out.println("내 나이는 :"+age);
+		System.out.println("내 이름는 :"+name);
+		System.out.println("내 키는 :"+height);
+    }
+
+}
